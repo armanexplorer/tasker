ed7bee754e3d02c6bf8f20d5e4ad8b6f65213062
armanexplorer <armanexplorer@gmail.com>
2021-02-16 12:23:11 +0330

    add headless mode to chrome browser to pass browser test

--- tasks/tests/test_browser.py
+++ tasks/tests/test_browser.py
@@ -4,7 +4,8 @@
 from django.test import TestCase
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
+# from pyvirtualdisplay import Display
 
 from pydjango_ci_integration.settings import SITE_URL
 
@@ -13,9 +14,19 @@ class TaskListViewTest(TestCase):
     """
     Test View class
     """
-    # # Browser Integration testing with Selenium
+    # Browser Integration testing with Selenium
     def test_chrome_site_homepage(self):
-        browser = webdriver.Chrome(ChromeDriverManager().install())
+        # display = Display(visible=0, size=(800, 800))
+        # display.start()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+        
+        # browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
         browser.get(SITE_URL)
         self.assertIn('Semaphore', browser.title)
         browser.close()
