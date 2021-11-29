a016a91c900ebe5bcdfaa1b5ab48c3e6f7279697
Arman <armanexplorer@gmail.com>
2021-11-29 05:29:12 +0330

    better UI + better tests

--- tasks/tests/test_browser.py
+++ tasks/tests/test_browser.py
@@ -1,30 +1,63 @@
-"""
-    Unit Test file for views
-"""
-from django.test import TestCase
-
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# from pyvirtualdisplay import Display
-
-from pydjango_ci_integration.settings import SITE_URL
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-class TaskListViewTest(TestCase):
+class SeleniumTest(StaticLiveServerTestCase):
     """
     Test View class
     """
-    # Browser Integration testing with Selenium
-    def test_chrome_site_homepage(self):
-        """
-        Simple test to check the correct loading of home page
-        """
+    browser = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--window-size=1420,1080')
-        chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
-        browser = webdriver.Chrome(chrome_options=chrome_options)
-        browser.get(SITE_URL)
-        self.assertIn('Semaphore', browser.title)
-        browser.close()
+        cls.browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        cls.browser.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    # Browser Integration testing with Selenium
+    def test_homepage_title(self):
+        """
+        Simple test to check the correct loading of home page
+        """
+        self.browser.get('%s%s' % (self.live_server_url, '/'))
+        self.assertIn('TDD', self.browser.title)
+
+    def test_create_task(self):
+        """
+        Simple test to check the correct constructing of new tasks
+        """
+        self.browser.get('%s%s' % (self.live_server_url, '/'))
+        add_button = self.browser.find_element_by_link_text('add_task')
+        add_button.click()
+
+        # store the number of tasks
+        carts = self.browser.find_elements_by_class_name('card-body in-list')
+        old_num = len(carts)
+
+        # enter title
+        title_input = self.browser.find_element_by_name('task_title')
+        title_input.send_keys('test title from selenium')
+
+        # enter description
+        desc_input = self.browser.find_element_by_name('task_description')
+        desc_input.send_keys('test description from selenium')
+
+        # click on create task button
+        create_btn = self.browser.find_element_by_tag_name('button[type="submit"]')
+        create_btn.click()
+
+        carts = self.browser.find_elements_by_class_name('card-body')
+        new_num = len(carts)
+
+        self.assertEqual(new_num, old_num + 1)
--- tasks/views.py
+++ tasks/views.py
@@ -15,15 +15,18 @@ class TaskList(ListView):
     Task list Generic List View
     """
     model = Task
-    ordering = ['-task_created']
+    # ordering = ['-task_created']
 
     def get_context_data(self, **kwargs):
         context = super(TaskList, self).get_context_data(**kwargs)
         context.update({'nlink': 'list'})
         return context
 
+    # def render_to_response(self, context, **response_kwargs):
+    #     super(TaskList, self).render_to_response()
 
-class TaskCreate(CreateView):
+
+class TaskCreate(CreateView, ListView):
     """
     Task list Generic Create View
     """
@@ -37,15 +40,16 @@ class TaskCreate(CreateView):
         return context
 
 
-class TaskDetails(DetailView):
+class TaskDetails(DetailView, ListView):
     """
     Task list Detail View
     """
+    object_list = Task.objects.all()
     model = Task
     fields = ['task_title', 'task_description', 'task_created', 'task_updated']
 
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(UpdateView, ListView):
     """
     Task list Update View
     """
@@ -59,20 +63,19 @@ class TaskUpdate(UpdateView):
         return context
 
 
-class TaskDelete(DeleteView):
+class TaskDelete(DeleteView, ListView):
     """
     Task list Delete View
     """
+    object_list = Task.objects.all()
     model = Task
     success_url = reverse_lazy('tasks:tasks_list')
 
 
 def task_default(request):
     """
-    this is default behavour when we hanv't id
+    this is default behaviour when there is no id
     """
-    #response = redirect(f'/task/{Task.objects.last().pk}')
-    #return response
     return TaskDetails.as_view()(request, pk=Task.objects.last().pk)
 
 
@@ -89,7 +92,8 @@ class Custom500(TemplateView):
     """
     template_name = 'tasks/500.html'
 
+
 def fake_view(request):
     from django.core.cache import cache
-    cache.set('ali', 1)
-    return HttpResponse(cache.get('ali'))
+    cache.set('key', 1)
+    return HttpResponse(cache.get('key'))
